7b2d9f55a08092a67ab25407d19b1113d9e62898
§Domenik Reitzner <d.reitzner@emakina.at>
2021-09-07 13:11:23 +0200

    try set up adapter tests

---
+++ examples/i18n/src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script type="typescript">
+    import {i18n, page} from '$app/stores';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<h2>current lang: {$page.lang}</h2>
+<p>{JSON.stringify($i18n, null, 2)}</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ examples/i18n/svelte.config.js
@@ -0,0 +1,27 @@
+import preprocess from 'svelte-preprocess';
+import adnetlify from '@sveltejs/adapter-netlify';
+import adnode from '@sveltejs/adapter-node';
+import adstatic from '@sveltejs/adapter-static';
+import advercel from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		i18n: {
+			defaultLocale: 'en',
+			locales: ['de']
+		},
+		adapter: adnetlify()
+		// adapter: adnode()
+		// adapter: adstatic()
+		// adapter: advercel()
+	}
+};
+
+export default config;
